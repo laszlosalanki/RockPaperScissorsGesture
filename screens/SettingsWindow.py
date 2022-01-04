ae151bc5f3354ebf7ee6d94c0638eb0f7564e0d7
László Salánki <laszlosalanki2001@gmail.com>
2022-01-04 01:40:00 +0100

    started solving logging setting, settings file, extended functionality of settings_file_helper.py

--- screens/SettingsWindow.py
+++ screens/SettingsWindow.py
@@ -1,4 +1,41 @@
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
+from kivy.uix.switch import Switch
+from os.path import exists
+from data import constants
+from settings_file_helper import create_settings_file, update_settings_file
+
+settings = dict()
+filename_with_path = constants.SETTINGS_FILE_RELATIVE_PATH + constants.SETTINGS_FILE_NAME
+
+if exists(filename_with_path):
+    with open(filename_with_path, 'r') as settings_file:
+        settings_lines = settings_file.readlines()[1:]
+
+        for line in settings_lines:
+            settings[line.split(' ')[0]] = line.split(' ')[1]
+
+        # TODO: set the switches
+    #
+else:
+    create_settings_file(filename_with_path, constants.SETTINGS_HEADER)
+
+
+class LoggingSwitch(Switch):
+    def switch_callback(self, active):
+        if active:
+            update_settings_file(filename_with_path, constants.SETTINGS_IS_LOGGING_ENABLED_KEY,
+                                 'True', constants.SETTINGS_HEADER)
+            settings[constants.SETTINGS_IS_LOGGING_ENABLED_KEY] = 'True'
+        else:
+            update_settings_file(filename_with_path, constants.SETTINGS_IS_LOGGING_ENABLED_KEY,
+                                 'False', constants.SETTINGS_HEADER)
+            settings[constants.SETTINGS_IS_LOGGING_ENABLED_KEY] = 'False'
+
+
+class BackAndSaveButton(Button):
+    def save_settings(self):
+        pass
 
 
 class SettingsWindow(Screen):
--- settings_file_helper.py
+++ settings_file_helper.py
@@ -2,7 +2,7 @@ from data import constants
 from os import remove
 
 
-def update_settings_file(file_with_path, key, value):
+def update_settings_file(file_with_path, key, value, header=constants.ACT_GAME_SETTINGS_HEADER):
     with open(file_with_path, 'r') as settings_file:
         lines = settings_file.readlines()[1:]
     #
@@ -16,7 +16,7 @@ def update_settings_file(file_with_path, key, value):
         #
 
         with open(file_with_path, 'w') as settings_file:
-            settings_file.write(constants.ACT_GAME_SETTINGS_HEADER + '\n')
+            settings_file.write(header + '\n')
             settings_file.writelines(lines)
         #
     else:
@@ -25,16 +25,14 @@ def update_settings_file(file_with_path, key, value):
         lines.append(key + str(value) + '\n')
 
         with open(file_with_path, 'w') as settings_file:
-            settings_file.write(constants.ACT_GAME_SETTINGS_HEADER + '\n')
+            settings_file.write(header + '\n')
             settings_file.writelines(lines)
         #
     #
-    print(constants.LOG_TEMPLATE, constants.LOG_ACT_GAME_SETTINGS_FILE_UPDATED)
 
 
-def create_settings_file(file_with_path):
+def create_settings_file(file_with_path, header=constants.ACT_GAME_SETTINGS_HEADER):
     with open(file_with_path, 'w') as settings_file:
-        settings_file.write(constants.ACT_GAME_SETTINGS_HEADER)
+        settings_file.write(header)
         settings_file.write('\n')
     #
-    print(constants.LOG_TEMPLATE, constants.LOG_ACT_GAME_SETTINGS_FILE_CREATED)
