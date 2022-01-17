c5af6afe3ddcb8889b31df1d4986bd2048c1dd62
László Salánki <laszlosalanki2001@gmail.com>
2022-01-17 13:10:48 +0100

    difficulty selection window removed

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from win32api import GetSystemMetrics
 from data import constants
 from screens.MainMenuWindow import MainMenuWindow
 from screens.NewGameWindow import NewGameWindow
-from screens.DifficultySelectionViewWindow import DifficultySelectionViewWindow
 from screens.OpponentSelectionViewWindow import OpponentSelectionViewWindow
 from screens.RoundSelectionViewWindow import RoundSelectionViewWindow
 from screens.GameWindow import GameWindow
