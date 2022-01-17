f82000fe1766d9dad72f1d9dcb36d8ec94ae3739
László Salánki <laszlosalanki2001@gmail.com>
2022-01-17 18:07:42 +0100

    resources optimized: game window events

--- screens/GameWindow.py
+++ screens/GameWindow.py
@@ -8,13 +8,32 @@ from hand_detection import HandDetection, get_coordinates_by_hand, recognise_han
 from cv2 import cv2
 
 detected_gesture = None
+player_1s_turn = True
 
 
 class CameraFrame(Image):
+    pass
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cap = cv2.VideoCapture(1)
+
+class GameWindow(Screen):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_once(self.init)
+
+    # Init images and labels
+    def init(self, dt):
+        # Camera feed
+        self.ids.camera_frame.source = constants.GESTURE_IMAGES[constants.LOG_NO_HAND]
+        # Detected images
+        self.ids.gesture_image_p1.source = constants.GESTURE_IMAGES[constants.LOG_NO_HAND]
+        self.ids.gesture_image_p2.source = constants.GESTURE_IMAGES[constants.LOG_NO_HAND]
+        # Detected labels
+        self.ids.gesture_text_p1.text = constants.LOG_NO_HAND
+        self.ids.gesture_text_p2.text = constants.LOG_NO_HAND
+
+    def on_pre_enter(self, *args):
+        self.cap = cv2.VideoCapture(0)
         self.hd = HandDetection()
         Clock.schedule_interval(self.update, 0.03)
 
@@ -36,20 +55,15 @@ class CameraFrame(Image):
             buf = buf1.tostring()
             texture = Texture.create(size=(processed_image.shape[1], processed_image.shape[0]), colorfmt='bgr')
             texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            self.texture = texture
 
+            self.ids.camera_frame.texture = texture
 
-class GameWindow(Screen):
-
-    def __init__(self, **kw):
-        super().__init__(**kw)
-        Clock.schedule_interval(self.predicted_photo_p1, 0.03)
-        Clock.schedule_interval(self.predicted_text_p1, 0.03)
-        # Clock.schedule_once(self.countdown, 10)
-
-        # TODO: game
-        Clock.schedule_once(self.predicted_photo_p2)
-        Clock.schedule_once(self.predicted_text_p2)
+            if player_1s_turn:
+                self.predicted_photo_p1()
+                self.predicted_text_p1()
+            else:
+                self.predicted_photo_p2()
+                self.predicted_text_p2()
 
     def countdown(self, dt):
         sec = 3
@@ -59,19 +73,27 @@ class GameWindow(Screen):
             sleep(1)
             sec -= 1
 
-    def predicted_photo_p1(self, dt):
+    def predicted_photo_p1(self):
         global detected_gesture
         if detected_gesture:
             if detected_gesture in constants.GESTURE_IMAGES.keys():
                 self.ids.gesture_image_p1.source = constants.GESTURE_IMAGES[detected_gesture]
 
-    def predicted_text_p1(self, dt):
+    def predicted_text_p1(self):
         global detected_gesture
         if detected_gesture:
             self.ids.gesture_text_p1.text = detected_gesture
 
-    def predicted_photo_p2(self, dt):
-        self.ids.gesture_image_p2.source = constants.GESTURE_IMAGES[constants.LOG_NO_HAND]
+    def predicted_photo_p2(self):
+        global detected_gesture
+        if detected_gesture:
+            if detected_gesture in constants.GESTURE_IMAGES.keys():
+                self.ids.gesture_image_p2.source = constants.GESTURE_IMAGES[detected_gesture]
 
-    def predicted_text_p2(self, dt):
-        self.ids.gesture_text_p2.text = 'No hand detected.'
+    def predicted_text_p2(self):
+        global detected_gesture
+        if detected_gesture:
+            self.ids.gesture_text_p2.text = detected_gesture
+
+    def on_pre_leave(self, *args):
+        self.cap.release()
